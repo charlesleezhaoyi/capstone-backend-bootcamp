b882bd878e99f0a67688cd2cb4af68423ff7495e
charlesleezhaoyi <charlesleezhaoyi@gmail.com>
2024-05-13 09:31:23 +0800

    commit before pulling latest test branch

--- controllers/nposController.ts
+++ controllers/nposController.ts
@@ -38,4 +38,93 @@ export class NposController {
       return res.status(400).json({ error: true, msg: (err as Error).message });
     }
   }
+
+  //missing endpoint
+  async createNpo(req: Request, res: Response) {
+    const {
+      name,
+      key_activities,
+      company_website_url,
+      country_incorporated,
+      company_description,
+      company_size,
+      company_logo_url,
+      acra_url,
+      open_ended_qn_1,
+      open_ended_qn_2,
+      open_ended_qn_3,
+      is_whitelabelled,
+      event_module,
+      discussion_module,
+      membership_mgmt,
+      is_verified,
+    } = req.body;
+    try {
+      const output = await Npos.create({
+        name: name,
+        key_activities: key_activities,
+        company_website_url: company_website_url,
+        country_incorporated: country_incorporated,
+        company_description: company_description,
+        company_size: company_size,
+        company_logo_url: company_logo_url,
+        acra_url: acra_url,
+        open_ended_qn_1: open_ended_qn_1,
+        open_ended_qn_2: open_ended_qn_2,
+        open_ended_qn_3: open_ended_qn_3,
+        is_whitelabelled: is_whitelabelled,
+        event_module: event_module,
+        discussion_module: discussion_module,
+        membership_mgmt: membership_mgmt,
+        is_verified: is_verified,
+      });
+      return res.json(output);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: (err as Error).message });
+    }
+  }
+
+  //missing endpoint
+  async updateNpoVerifiedStatus(req: Request, res: Response) {
+    const { npo_id } = req.body;
+    try {
+      const npo = await Npos.findByPk(npo_id);
+      if (npo) {
+        npo.is_verified = true;
+        await npo.save();
+      }
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: (err as Error).message });
+    }
+  }
+
+  async updateNpoData(req: Request, res: Response) {
+    const { npo_id, ...updateData } = req.body;
+    try {
+      const npo = await Npos.findByPk(npo_id);
+      if (!npo) {
+        return res.status(404).json({ error: true, msg: "NPO not found" });
+      }
+
+      // Update the NPO data with the user input
+      let isChanged = false;
+      for (const key in updateData) {
+        if (
+          updateData.hasOwnProperty(key) &&
+          (npo as any)[key] !== updateData[key]
+        ) {
+          (npo as any)[key] = updateData[key];
+          isChanged = true;
+        }
+      }
+
+      if (isChanged) {
+        await npo.save();
+      }
+
+      return res.json({ success: true, msg: "NPO data updated successfully" });
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: (err as Error).message });
+    }
+  }
 }
