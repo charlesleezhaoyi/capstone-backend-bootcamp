803b7c4aee23f3081a66fcf56d32b47c055b4cdf
patrickkok <pkok1996@gmail.com>
2024-05-16 23:55:40 +0800

    Add columns to Events, update seeder accordingly. Update EventsController

--- db/seeders/20240514133329-events.ts
+++ db/seeders/20240514133329-events.ts
@@ -8,21 +8,27 @@ module.exports = {
     await queryInterface.bulkInsert("events", [
       {
         organiser_id: 1,
+        npo_id: 1,
         event_overview: "Come clean the beaches of Singapore with us",
         event_name: "Beach Cleanup",
         event_photo_url:
           "https://aebc975c.rocketcdn.me/wp-content/uploads/2020/12/plage.jpg",
         date: new Date(),
         time: "08:00AM",
+        location: "East Coast Park Carpark A",
+        price: 0,
         created_at: new Date(),
         updated_at: new Date(),
       },
       {
         organiser_id: 1,
+        npo_id: 1,
         event_overview: "Join us at our annual members meeting",
         event_name: "Annual Party",
         date: new Date(),
         time: "07:00PM",
+        location: "Sentosa Beach Club",
+        price: 30,
         created_at: new Date(),
         updated_at: new Date(),
       },
