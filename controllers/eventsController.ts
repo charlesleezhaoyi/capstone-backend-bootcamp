22df567068762438702ded1eb0333ddf043db4d3
patrickkok <pkok1996@gmail.com>
2024-05-16 23:56:32 +0800

    Remove unused function in EventsController

--- controllers/eventsController.ts
+++ controllers/eventsController.ts
@@ -4,10 +4,6 @@ import { Events, Members, NpoMembers, Npos, Roles } from "../db/models/index";
 export class EventsController {
   async getNpoEvents(req: Request, res: Response) {
     const { npoId } = req.params;
-    const fetchOrganiser = async (event: Events) => {
-      const organiser = await Members.findByPk(event.organiser_id);
-      return { ...event.dataValues, organiser: organiser?.dataValues };
-    };
     try {
       const events = await Events.findAll({
         where: { npo_id: npoId },
